d5c8d6619a3b88416b155a52d0e4d969ac5045b7
Glenn <glennchiang24@gmail.com>
2026-06-07 19:05:10 +0800

    Add dataaccess for meal logs

--- src/handlers/mealLogs.ts
+++ src/handlers/mealLogs.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from "express";
+import { getMealLogs } from "../dataaccess/mealLogs";
 
 export async function handleGetMealLogs(req: Request, res: Response) {
-  
+  const userId = Number(req.params.id)
 }
 
 export async function handleCreateMealLog(req: Request, res: Response) {
