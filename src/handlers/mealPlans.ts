e0f8c588a9f5378f8947457386dc3deafd37e1c7
Glenn <glennchiang24@gmail.com>
2026-06-09 13:25:32 +0800

    Implement handlers for meal plans

--- src/handlers/mealPlans.ts
+++ src/handlers/mealPlans.ts
@@ -1,27 +1,37 @@
 import { Request, Response } from "express";
-import { mealsService } from '../services/meals';
+import { mealPlansService } from "../services/mealPlans";
 
 export async function handleGetSampleMealPlans(req: Request, res: Response) {
-  
+  const mealPlans = await mealPlansService.getSampleMealPlans();
+  res.json(mealPlans);
 }
 
 // Get meal plans created by the given user
 export async function handleGetUserMealPlans(req: Request, res: Response) {
-  
+  const userId = Number(req.params.id);
+  const mealPlans = await mealPlansService.getUserMealPlans(userId);
+  res.json(mealPlans);
 }
 
 export async function handleGetMealPlan(req: Request, res: Response) {
-  
+  const mealPlanId = Number(req.params.id);
+  const mealPlan = await mealPlansService.getMealPlan(mealPlanId);
+  res.json(mealPlan);
 }
 
 export async function handleCreateMealPlan(req: Request, res: Response) {
-  
+  const mealPlan = await mealPlansService.createMealPlan(req.body);
+  res.json(mealPlan);
 }
 
 export async function handleUpdateMealPlan(req: Request, res: Response) {
-  
+  const mealPlanId = Number(req.params.id);
+  const mealPlan = await mealPlansService.updateMealPlan(req.body);
+  res.json(mealPlan);
 }
 
 export async function handleDeleteMealPlan(req: Request, res: Response) {
-  
+  const mealPlanId = Number(req.params.id);
+  await mealPlansService.deleteMealPlan(mealPlanId);
+  res.json({ message: `Deleted meal plan: ${mealPlanId}` });
 }
