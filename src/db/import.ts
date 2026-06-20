60b88ccc1947614ce8cf074f191da576c36a6441
Glenn <glennchiang24@gmail.com>
2026-06-20 12:34:30 +0800

    Update import script to add food units

--- src/db/import.ts
+++ src/db/import.ts
@@ -1,45 +1,52 @@
 import fs from "node:fs/promises";
 import db from "./db";
-import { foodsTable } from "./schema";
+import { foodsTable, foodUnitsTable } from "./schema";
 
 async function main() {
   const baseFoodFile = await fs.readFile("data/foundation_food.json", "utf8");
   const baseFoods = JSON.parse(baseFoodFile)["FoundationFoods"];
 
-  const batchSize = 500;
+  for (let i = 0; i < baseFoods.length; i++) {
+    const food = baseFoods[i];
 
-  for (let i = 0; i < baseFoods.length; i += batchSize) {
-    const batch = baseFoods.slice(i, i + batchSize);
-
-    const values = batch.map((food: any) => {
-      const nutrients = food["foodNutrients"] ?? [];
-      const calories =
-        nutrients.find((nutrient: any) => nutrient.nutrient.id == 2047)
-          ?.amount ?? 0;
-      const protein =
-        nutrients.find((nutrient: any) => nutrient.nutrient.id == 1003)
-          ?.amount ?? 0;
-      const fat =
-        nutrients.find((nutrient: any) => nutrient.nutrient.id == 1004)
-          ?.amount ?? 0;
-      const carbs = Math.max(
-        nutrients.find((nutrient: any) => nutrient.nutrient.id == 1005)
-          ?.amount ?? 0,
+    const nutrients = food["foodNutrients"] ?? [];
+    const calories =
+      nutrients.find((nutrient: any) => nutrient.nutrient.id == 2047)?.amount ??
+      0;
+    const protein =
+      nutrients.find((nutrient: any) => nutrient.nutrient.id == 1003)?.amount ??
+      0;
+    const fat =
+      nutrients.find((nutrient: any) => nutrient.nutrient.id == 1004)?.amount ??
+      0;
+    const carbs = Math.max(
+      nutrients.find((nutrient: any) => nutrient.nutrient.id == 1005)?.amount ??
         0,
-      );
+      0,
+    );
 
-      return {
-        id: food["fdcId"],
-        name: food["description"],
-        defaultServingSize: food["foodPortions"]?.[0]?.["gramWeight"] ?? 0,
-        calories,
-        carbs,
-        fat,
-        protein,
-      };
+    const foodId = food["fdcId"];
+
+    await db.insert(foodsTable).values({
+      id: foodId,
+      name: food["description"],
+      defaultServingSize: food["foodPortions"]?.[0]?.["gramWeight"] ?? 0, // TODO: Fix this to find RACC
+      calories,
+      carbs,
+      fat,
+      protein,
     });
 
-    await db.insert(foodsTable).values(values);
+    // Food units
+    for (const foodPortion of food["foodPortions"]) {
+      const unitId = foodPortion.measureUnit.id;
+      const gramsPerUnit = foodPortion.gramWeight / foodPortion.amount;
+      await db.insert(foodUnitsTable).values({
+        foodId,
+        unitId,
+        gramsPerUnit,
+      });
+    }
   }
 }
 
